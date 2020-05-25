35f2a1fc0d37c69ddb46ae2df2001ae9d05aae06
Dev <dvptl68@gmail.com>
2020-05-25 08:02:44 -0400

    Import scripts

---
+++ scripts/updateBatch.cmd
@@ -0,0 +1,12 @@
+@echo off
+set /p message="Enter the commit message: "
+set /p confirm="Would you like to commit and push all changes with the following message: %message% (y/n)? ";
+if "%confirm%"=="y" (
+  cd ..
+  git add .
+  git commit -m "%message%"
+  git push
+  echo Staged, committed, and pushed changes!
+) else (
+  echo Process terminated.
+)

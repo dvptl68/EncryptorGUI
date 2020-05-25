2865cfd011fefaab9e50591e779ea5cf6234c1f6
Dev Patel <dvptl68@gmail.com>
2020-05-25 09:11:49 -0400

    Add start button functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+//Constructor for main window
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow) {
+    //Create main window
     ui->setupUi(this);
+
 }
 
+//Destructor for main window
 MainWindow::~MainWindow() {
+    //Delete main window
     delete ui;
 }
 
+//Function to execute when start is clicked
+void MainWindow::on_startButton_clicked() {
+    //Create new font for title
+    QFont serifFont("OCR A Extended", 20, QFont::Bold);
+    serifFont.kerning();
+    //Set font and position of title
+    ui->titleLabel->setFont(serifFont);
+    ui->titleLabel->setGeometry(8, 0, 200, 45);
+    //Delete start button
+    ui->startButton->deleteLater();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_startButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };

e2b31f8f2365d9a4f551ddd2fd092e637909e554
Dev Patel <dvptl68@gmail.com>
2020-05-26 09:36:26 -0400

    Design drive select layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "DriveSelect.hpp"
-#include <map>
+#include <vector>
 #include <string>
+#include <QListWidget>
+#include <QPushButton>
 
 using namespace std;
 
@@ -10,7 +12,6 @@ using namespace std;
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow) {
     //Create main window
     ui->setupUi(this);
-
 }
 
 //Destructor for main window
@@ -24,13 +25,24 @@ void MainWindow::on_startButton_clicked() {
     //Create new font for title
     QFont serifFont("OCR A Extended", 20, QFont::Bold);
     serifFont.kerning();
-    //Set font and position of title
+    //Set font and new position of title
     ui->titleLabel->setFont(serifFont);
     ui->titleLabel->setGeometry(8, 0, 200, 45);
     //Delete start button
-//    ui->startButton->deleteLater();
+    ui->startButton->deleteLater();
+    //Get list of available drives
     DriveSelect d;
-    QString t = "";
-    t.setNum(d.getDrives().size());
-    ui->startButton->setText(t);
+    vector<string> drives = d.getDrives();
+    //Create and show list widget in center of screen
+    QListWidget *dr = new QListWidget(ui->centralwidget);
+    dr->setGeometry(350, 200, 300, 300);
+    dr->show();
+    //Create and show next push button
+    QPushButton *next = new QPushButton(ui->centralwidget);
+    next->setGeometry(886, 651, 104, 39);
+    QFont font("Seqoe MDL2 Assets", 20);
+    font.kerning();
+    next->setFont(font);
+    next->setText("Next");
+    next->show();
 }

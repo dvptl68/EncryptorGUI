722394128f32f71e6510eb2d28d2520bc6bd164d
Dev Patel <dvptl68@gmail.com>
2020-05-26 13:12:09 -0400

    Complete second page layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,40 +24,47 @@ MainWindow::~MainWindow() {
 //Function to execute when start is clicked
 void MainWindow::on_startButton_clicked() {
 
-  //Delete all elements in grid layout
-//  if (ui->pageLayout->layout() != NULL){
-//      QLayoutItem* item;
-//      while ((item = ui->pageLayout->layout()->takeAt(0)) != NULL){
-//          delete item->widget();
-//          delete item;
-//      }
-//      delete ui->pageLayout->layout();
-//  }
+  //Change size of rows 2 and 3 to fit list
+  ui->pageLayout->setRowStretch(1, 50);
+  ui->pageLayout->setRowStretch(2, 200);
 
-  //Create new font for title
+  //Create new font for title and place in top left
   QFont serifFont("OCR A Extended", 20, QFont::Bold);
   serifFont.kerning();
-  QLabel *title = new QLabel(ui->centralwidget);
+  QLabel *title = new QLabel(ui->cont1);
   //Set font and new position of title
   title->setFont(serifFont);
   title->setText("Encryptor");
   title->resize(200, 30);
   title->show();
-  ui->pageLayout->addWidget(title, 0, 0);
 
-//  //Get list of available drives
-//  DriveSelect d;
-//  vector<string> drives = d.getDrives();
-//  //Create and show list widget in center of screen
-//  QListWidget *dr = new QListWidget(ui->centralwidget);
-//  dr->setGeometry(350, 200, 300, 300);
-//  dr->show();
-//  //Create and show next push button
-//  QPushButton *next = new QPushButton(ui->centralwidget);
-//  next->setGeometry(886, 651, 104, 39);
-//  QFont font("Seqoe MDL2 Assets", 20);
-//  font.kerning();
-//  next->setFont(font);
-//  next->setText("Next");
-//  next->show();
+  //Delete old title and start button
+  ui->titleLabel->deleteLater();
+  ui->startButton->deleteLater();
+
+  //Get list of available drives
+  DriveSelect d;
+  vector<string> drives = d.getDrives();
+
+  //Create and show list widget in center of screen
+  QListWidget *dr = new QListWidget(ui->cont8);
+  dr->setGeometry(144, 0, 300, 200);
+  dr->show();
+
+  //Create and show next push button
+  QPushButton *next = new QPushButton(ui->cont12);
+  next->setGeometry(85, 105, 100, 40);
+  QFont font1("Seqoe MDL2 Assets", 20);
+  font1.kerning();
+  next->setFont(font1);
+  next->setText("Next");
+  next->show();
+
+  //Create and show instruction label
+  QLabel *instruct = new QLabel(ui->cont5);
+  QFont font2("Seqoe MDL2 Assets", 25);
+  instruct->setFont(font2);
+  instruct->setText("Select a drive:");
+  instruct->move(185, 30);
+  instruct->show();
 }

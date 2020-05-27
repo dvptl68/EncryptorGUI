acec647db02995cc5468d5dfc2cdc75a82c75138
Dev Patel <dvptl68@gmail.com>
2020-05-27 08:32:45 -0400

    Fix drive select bug

--- DriveSelect.cpp
+++ DriveSelect.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
-#include <algorithm>
 #include <windows.h>
 #include <sys/stat.h>
-#include <map>
+#include <vector>
 #include <string>
 #include "DriveSelect.hpp"
-#include <QTextStream>
 
 using namespace std;
 struct stat info;
 
 //Constructor for Select class
 DriveSelect::DriveSelect(void){
-  QTextStream out(stdout);
   //Fill vector with possible drive letters
   vector<string> alphabet = {"A", "B", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
   //Iterate through possible drive letters
   for (int i = 0; i < (int)alphabet.size(); i++){
     struct stat buffer;
     //Set current path
-    char current[3] ={alphabet[i].c_str()[0], ':', '\\'};
+    char current[4] ={alphabet[i].c_str()[0], ':', '\\'};
     //Check if drive path exists
     if (stat (current, &buffer) == 0){
-      out << "A";
       //Retrieve drive name and add to vector
       char VolumeName[MAX_PATH];
       GetVolumeInformationA((LPCSTR)current, (LPSTR)VolumeName, sizeof(VolumeName), NULL, NULL, NULL, NULL, 0);
